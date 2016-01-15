138298dd18fd4872fb7a2d0880065490854b2b55
Sam Hamann <shama_000@192.168.2.68>
2016-01-15 15:36:11 -0600

    Added Drive command and subsystem

---
+++ Herdbot2016/src/org/usfirst/frc/team5542/robot/subsystems/DriveTrain.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team5542.robot.subsystems;
+
+import org.usfirst.frc.team5542.robot.RobotMap;
+import org.usfirst.frc.team5542.robot.commands.TankDrive;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	private RobotDrive myDrive;
+	private CANTalon lfMotor, rfMotor, lbMotor, rbMotor;
+
+	private DriveTrain(){
+		lfMotor = new CANTalon(RobotMap.lfMotor);
+		rfMotor = new CANTalon(RobotMap.rfMotor);
+		lbMotor = new CANTalon(RobotMap.lbMotor);
+		rbMotor = new CANTalon(RobotMap.rbMotor);
+		myDrive = new RobotDrive(lfMotor, rfMotor, lbMotor, rbMotor);
+	}
+	
+	public static DriveTrain instance;
+	
+	public static DriveTrain getInstance(){
+		if (instance == null)
+			instance = new DriveTrain();
+		return instance;
+	}
+	
+	public void tankDrive(double left, double right){
+		
+	}
+		
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        setDefaultCommand(new TankDrive());
+    }
+}
+
