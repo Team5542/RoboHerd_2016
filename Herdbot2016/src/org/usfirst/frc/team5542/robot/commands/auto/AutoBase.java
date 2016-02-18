e5e77e0f8934ccbba4177de90fbe44f1262add20
weeryan17 <theweeryan17@gmail.com>
2016-02-18 16:29:48 -0600

    started work on the auto. nothing is acctuly hapening yet

---
+++ Herdbot2016/src/org/usfirst/frc/team5542/robot/commands/auto/AutoBase.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team5542.robot.commands.auto;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public abstract class AutoBase extends Command {
+	
+	public void init(){
+		
+	}
+}
