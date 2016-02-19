b5a8dfbabacab394e26bf17055fe915dbb37c40f
weeryan17 <theweeryan17@gmail.com>
2016-02-18 18:12:05 -0600

    Removed the timer.delay in hopes of getting things working

--- Herdbot2016/src/org/usfirst/frc/team5542/robot/commands/CameraRefresher.java
+++ Herdbot2016/src/org/usfirst/frc/team5542/robot/commands/CameraRefresher.java
@@ -18,7 +18,6 @@ public class CameraRefresher extends CommandBase {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	cameraR.refreshCamera();
-    	Timer.delay(.5);
     }
 
     // Make this return true when this Command no longer needs to run execute()
