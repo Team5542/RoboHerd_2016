2fe9829b63bcbdd259cc498decf00f5958b937c8
weeryan17 <theweeryan17@gmail.com>
2016-02-21 16:26:57 -0600

    forgot to acctuly do stuff with arm

--- Herdbot2016/src/org/usfirst/frc/team5542/robot/commands/auto/Calebrate.java
+++ Herdbot2016/src/org/usfirst/frc/team5542/robot/commands/auto/Calebrate.java
@@ -15,7 +15,7 @@ public class Calebrate extends AutoBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	arm.getInstance().
+    	arm.move(.5);
     }
 
     // Make this return true when this Command no longer needs to run execute()
